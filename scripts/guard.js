3592a7e19295e424cdc3ae5d4391e8d4014eb3fc
tonkec <antonija1023@gmail.com>
2024-01-31 13:49:41 +0100

    improve guard

--- scripts/guard.js
+++ scripts/guard.js
@@ -1,16 +1,37 @@
 import { onAuthStateChanged } from "firebase/auth";
 import {auth} from "./firebase.js";
+const newRecipeButton = document.getElementById('new-recipe-button');
+const isRootRoute = window.location.pathname === '/' || window.location.pathname === '/index.html';
+const isProtectedRoute = window.location.pathname === '/new.html';
 
-const isUserLoggedIn = ( ) => {
-   onAuthStateChanged(auth, (user) => {
-        if (user) {
-            console.log('user is logged in');
-        } else {
-            console.log('user is not logged in');
-            window.location.href = '/';
-        }
+const checkUser = (authCallback, nonAuthCallback) => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log('user is logged in');
+      authCallback()
+    } else {
+      console.log('user is not logged in');
+      nonAuthCallback()
     }
-    );
+  });
 }
 
-window.addEventListener('DOMContentLoaded', isUserLoggedIn);
+const showButton = () => {
+  newRecipeButton.style.display = 'inline';
+}
+
+const hideButton = () => {
+  newRecipeButton.style.display = 'none';
+}
+
+const reidrectToRootRoute = () => {
+  window.location.href = '/';
+}
+
+if (isRootRoute) {
+  checkUser(showButton, hideButton);
+}
+
+if (isProtectedRoute) {
+  checkUser(() => {}, reidrectToRootRoute);
+}
