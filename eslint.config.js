e2267435a0da50faa64465386a8ee5afddaa9d0a
tonkec <antonija1023@gmail.com>
2024-01-31 12:10:44 +0100

    add vite

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  ignores: [
+    'dist',
+    'node_modules',
+  ],
+  rules: {
+    'style/brace-style': ['error', '1tbs'],
+    'style/arrow-parens': ['error', 'always'],
+    'curly': ['error', 'all'],
+    'antfu/consistent-list-newline': 'off',
+  },
+})
