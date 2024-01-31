271e72371cb77c80dcb3de714853dcaa0b5e40b9
tonkec <antonija1023@gmail.com>
2024-01-31 12:52:06 +0100

    add auth with firebase

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,44 @@
-console.log("hello")
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+import {auth} from "./firebase.js";
+const signUpForm = document.getElementById("signup-form");
+const loginForm = document.getElementById("login-form");
+
+const onLoginSubmit = (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        })
+        .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+    });
+}
+const onCreateAccountSubmit = (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        })
+        .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+    });
+}
+
+
+if (signUpForm) {
+    signUpForm.addEventListener('submit', onCreateAccountSubmit);
+}
+
+if (loginForm) {
+    loginForm.addEventListener('submit', onLoginSubmit);    
+}
